b2622b52e97c7e1e6fc87f5e58b771b8b4c527eb
Sean Pleaner <sean@apexcrm.co.za>
2023-05-12 23:41:27 +0100

    Add function to calc H index

--- backend/contracts/PublicationReferenceModule.sol
+++ backend/contracts/PublicationReferenceModule.sol
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.10;
 
-import {IReferenceModule} from "../../../interfaces/IReferenceModule.sol";
-import {ModuleBase} from "../ModuleBase.sol";
-import {FollowValidationModuleBase} from "../FollowValidationModuleBase.sol";
+import {IReferenceModule} from "@aave/lens-protocol/contracts/interfaces/IReferenceModule.sol";
+import {ModuleBase} from "@aave/lens-protocol/contracts/core/modules/ModuleBase.sol";
+import {FollowValidationModuleBase} from "@aave/lens-protocol/contracts/core/modules/FollowValidationModuleBase.sol";
 import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 /**
@@ -14,7 +14,13 @@ import {IERC721} from "@openzeppelin/contracts/token/ERC721/IERC721.sol";
  * by a follower.
  */
 contract FollowerOnlyReferenceModule is FollowValidationModuleBase, IReferenceModule {
+    // Mapping of Publicaiton Ids to an Array of Paublication IDs its mentioned in
+    mapping(uint => uint[]) profileIdToPublicationIds;
+    mapping(uint => uint[]) publicationIdToMonthionIds;
+
     constructor(address hub) ModuleBase(hub) {}
+
+    event Publish(uint256 profileId, uint256 pubId, uint[] citeIds);
 
     /**
      * @dev There is nothing needed at initialization.
@@ -23,7 +29,11 @@ contract FollowerOnlyReferenceModule is FollowValidationModuleBase, IReferenceMo
         uint256 profileId,
         uint256 pubId,
         bytes calldata data
-    ) external pure override returns (bytes memory) {
+    ) external override returns (bytes memory) {
+        uint[] memory citeIds = abi.decode(data, (uint[]));
+        publicationIdToMonthionIds[pubId] = citeIds;
+        profileIdToPublicationIds[profileId].push(pubId);
+        emit Publish(profileId, pubId, citeIds);
         return new bytes(0);
     }
 
@@ -40,6 +50,8 @@ contract FollowerOnlyReferenceModule is FollowValidationModuleBase, IReferenceMo
     ) external view override {
         address commentCreator = IERC721(HUB).ownerOf(profileId);
         _checkFollowValidity(profileIdPointed, commentCreator);
+
+        //to do only H Indedx > 2
     }
 
     /**
@@ -55,5 +67,19 @@ contract FollowerOnlyReferenceModule is FollowValidationModuleBase, IReferenceMo
     ) external view override {
         address mirrorCreator = IERC721(HUB).ownerOf(profileId);
         _checkFollowValidity(profileIdPointed, mirrorCreator);
+    }
+
+    function calcHIndex(uint256 profileId) private view returns (uint) {
+        uint hIndex = 0;
+        uint[] memory pubs = profileIdToPublicationIds[profileId];
+        uint totalPubs = 0;
+
+        for (uint i = 0; i < pubs.length; i++) {
+            if (publicationIdToMonthionIds[pubs[i]].length >= totalPubs) {
+                hIndex++;
+            }
+        }
+
+        return hIndex;
     }
 }
--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -38,7 +38,7 @@ module.exports = {
     solidity: {
         compilers: [
             {
-                version: "0.8.18",
+                version: "0.8.10",
             },
             {
                 version: "0.6.6",
