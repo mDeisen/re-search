2411e9d3e5743b1e79078ef774745a8c719b9fa4
Nezzar KEFIF <nezz0746@gmail.com>
2023-05-14 08:15:38 +0100

    feat: get lens content

--- frontend/pages/publication/[id].tsx
+++ frontend/pages/publication/[id].tsx
@@ -1,10 +1,15 @@
+import { useGetPublicationQuery } from "@/rtk/lenspub.api";
+import classNames from "classnames";
 import { useRouter } from "next/router";
 import { useState } from "react";
 
 const PublicationPage = () => {
   const router = useRouter();
+  const { id } = router.query;
+  const { data } = useGetPublicationQuery(id as string);
 
-  let response = { __html: "<p>some html</p>" };
+  // @ts-ignore
+  let response = { __html: data?.lensPub.metadata.content };
 
   const [reviewText, setReviewText] = useState("");
   const [accepted, setAccepted] = useState(false);
@@ -24,48 +29,46 @@ const PublicationPage = () => {
   };
 
   return (
-    <div>
-      <div
-        className="max-w-lg mx-auto"
-        dangerouslySetInnerHTML={{
-          __html: "<h1>The Content</h1><p>asdf asdf</p>",
-        }}
-      />
-      <form onSubmit={handleSubmit} className="max-w-lg mx-auto">
-        <br />
-        <hr />
-        <label
-          htmlFor="review-text"
-          className="block font-medium text-gray-700 mb-2"
-        >
-          Review
-        </label>
-        <textarea
-          id="review-text"
-          name="review-text"
-          placeholder="Enter review here..."
-          value={reviewText}
-          onChange={handleTextChange}
-          className="textarea w-full"
-          rows={4}
-        />
-
-        <div className="form-control">
-          <label className="label cursor-pointer">
-            <span className="label-text">Accept Article for Publication</span>
-            <input
-              type="checkbox"
-              className="toggle"
-              onChange={handleToggleChange}
-              checked={accepted}
-            />
+    <div className="flex flex-col justify-center items-center">
+      <div className="w-2/3">
+        <h1 className="text-2xl border">{data?.graphPub.title}</h1>
+        <div className=" border mx-auto" dangerouslySetInnerHTML={response} />
+        <form onSubmit={handleSubmit} className="">
+          <br />
+          <hr />
+          <label
+            htmlFor="review-text"
+            className="block font-medium text-gray-700 mb-2"
+          >
+            Review
           </label>
-        </div>
+          <textarea
+            id="review-text"
+            name="review-text"
+            placeholder="Enter review here..."
+            value={reviewText}
+            onChange={handleTextChange}
+            className="textarea w-full"
+            rows={4}
+          />
 
-        <button type="submit" className={`btn btn-outline btn-primary`}>
-          Submit Review
-        </button>
-      </form>
+          <div className="form-control">
+            <label className="label cursor-pointer">
+              <span className="label-text">Accept Article for Publication</span>
+              <input
+                type="checkbox"
+                className={classNames("toggle", accepted && "toggle-success")}
+                onChange={handleToggleChange}
+                checked={accepted}
+              />
+            </label>
+          </div>
+
+          <button type="submit" className={`btn btn-outline btn-primary`}>
+            Submit Review
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
--- frontend/rtk/lenspub.api.ts
+++ frontend/rtk/lenspub.api.ts
@@ -2,34 +2,44 @@ import { createApi } from "@reduxjs/toolkit/query/react";
 import { graphqlBaseQuery } from "./helpers";
 import { subgraphUrl } from "@/constants";
 import { gql } from "graphql-request";
-import { LensClient, PublicationFragment, development } from "@lens-protocol/client";
+import {
+  LensClient,
+  PublicationFragment,
+  development,
+} from "@lens-protocol/client";
 import { BigNumber } from "ethers";
 
 type LenspubPublication = {
+  id: string;
+  title: string;
+  profile: {
     id: string;
-    title: string;
-    profile: {
-        id: string;
-    }
-    citedPublications: {
-        id: string;
-    }[]
-}
+  };
+  citedPublications: {
+    id: string;
+  }[];
+};
 
-export type MergedPublication = { graphPub: LenspubPublication; lensPub: PublicationFragment };
+export type MergedPublication = {
+  graphPub: LenspubPublication;
+  lensPub: PublicationFragment;
+};
 
 export const publicationsAPI = createApi({
   reducerPath: "publications",
   baseQuery: graphqlBaseQuery({ baseUrl: subgraphUrl }),
   endpoints: (builder) => ({
-    listPublications: builder.query<MergedPublication[], {titleSearchString: string}>({
+    listPublications: builder.query<
+      MergedPublication[],
+      { titleSearchString: string }
+    >({
       queryFn: async (
         { titleSearchString },
         { signal, dispatch, getState },
         extraOptions,
         baseQuery
       ) => {
-        const pubs = await baseQuery({
+        const pubs = (await baseQuery({
           body: gql`
             query ListPublications($titleSearchString: String!) {
               publications(
@@ -48,8 +58,8 @@ export const publicationsAPI = createApi({
             }
           `,
           variables: { titleSearchString },
-        //   @ts-ignore
-        }).then((res) => res.data.publications) as LenspubPublication[];
+          //   @ts-ignore
+        }).then((res) => res.data.publications)) as LenspubPublication[];
 
         console.log(pubs);
 
@@ -57,43 +67,47 @@ export const publicationsAPI = createApi({
         development.name = "staging";
         // @ts-ignore
         development.url = "https://api-sandbox-mumbai.lens.dev";
-  
+
         const lensClient = new LensClient({
           environment: development,
         });
 
-        const publicationIds = pubs.map(({id, profile}) => {
-          const publicationId = BigNumber.from(id).mod(BigNumber.from("1000000")).toHexString();
-          const profileId = BigNumber.from(id).div(BigNumber.from("1000000")).toHexString();
+        const publicationIds = pubs.map(({ id, profile }) => {
+          const publicationId = BigNumber.from(id)
+            .mod(BigNumber.from("1000000"))
+            .toHexString();
+          const profileId = BigNumber.from(id)
+            .div(BigNumber.from("1000000"))
+            .toHexString();
 
           return profileId + "-" + publicationId;
         });
 
-
-        const lensPubs = await lensClient.publication.fetchAll({
-            publicationIds
-        }).then((res) => res.items);
+        const lensPubs = await lensClient.publication
+          .fetchAll({
+            publicationIds,
+          })
+          .then((res) => res.items);
 
         const mergePubs: MergedPublication[] = pubs.map((pub, i) => ({
-            graphPub: pub,
-            lensPub: lensPubs[i]
-        }))
+          graphPub: pub,
+          lensPub: lensPubs[i],
+        }));
 
         // Sort by lensPub.createdAt
         mergePubs.sort((a, b) => {
-            if (a.lensPub.createdAt < b.lensPub.createdAt) {
-                return -1;
-            } else if (a.lensPub.createdAt > b.lensPub.createdAt) {
-
-                return 1;
-            } else {
-                return 0;
-            }
-        })
+          if (a.lensPub.createdAt < b.lensPub.createdAt) {
+            return -1;
+          } else if (a.lensPub.createdAt > b.lensPub.createdAt) {
+            return 1;
+          } else {
+            return 0;
+          }
+        });
 
         return {
-            data: mergePubs,
-        }
+          data: mergePubs,
+        };
       },
     }),
     getPublication: builder.query<MergedPublication, string>({
@@ -103,7 +117,7 @@ export const publicationsAPI = createApi({
         extraOptions,
         baseQuery
       ) => {
-        const pub = await baseQuery({
+        const pub = (await baseQuery({
           body: gql`
             query GetPublication($publicationId: String!) {
               publication(id: $publicationId) {
@@ -119,33 +133,41 @@ export const publicationsAPI = createApi({
             }
           `,
           variables: { publicationId },
-        //   @ts-ignore
-        }).then((res) => res.data.publication) as LenspubPublication;
-
-        console.log(pub);
+          //   @ts-ignore
+        }).then((res) => res.data.publication)) as LenspubPublication;
 
         // @ts-ignore
         development.name = "staging";
         // @ts-ignore
         development.url = "https://api-sandbox-mumbai.lens.dev";
-  
+
         const lensClient = new LensClient({
           environment: development,
         });
 
+        const lensPublicationId = BigNumber.from(publicationId)
+          .mod(BigNumber.from("1000000"))
+          .toHexString();
+
+        const id =
+          BigNumber.from(pub.profile.id).toHexString() +
+          "-" +
+          lensPublicationId;
+
         const lensPub = await lensClient.publication.fetch({
-            publicationId
+          publicationId: id,
         });
 
         return {
-            data: {
-                graphPub: pub,
-                lensPub
-            },
-        }
-      }
-    })
+          data: {
+            graphPub: pub,
+            lensPub,
+          },
+        };
+      },
+    }),
   }),
 });
 
-export const { useListPublicationsQuery, useGetPublicationQuery } = publicationsAPI;
+export const { useListPublicationsQuery, useGetPublicationQuery } =
+  publicationsAPI;
